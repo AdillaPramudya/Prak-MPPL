d30cc6129513a39f133542b3962050601ec664f8
AdillaPramudya <107919439+AdillaPramudya@users.noreply.github.com>
2022-06-27 19:41:50 +0700

    Add files via upload

---
+++ bangunRuang.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public abstract class bangunRuang {
+    int a,b,c;
+    abstract public void printarea();
+    abstract public void printvolume();
+    public abstract void printketerangan();
+   }
